842739aab92187ff7b953d9cbf0e1c6f2e2ec234
Efrain Gonzalez <efrain800@gmail.com>
2018-07-25 18:17:15 -0700

    Added search bar class

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-  <div class="container">
-  <div class="row">
-    <div class="col-sm">
-      One of three columns
+ class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      tag: ''
+    }
+  }
+
+  setTag(tag){
+    this.setState({
+      tag: tag
+    })
+  }
+
+  render(){
+    return(
+    <div className="App">
+       <Tagbar onClick={(tag) => this.setTag(tag)}/> 
+       <p>{this.state.tag}</p>
     </div>
-    <div class="col-sm">
-      One of three columns
-    </div>
-    <div class="col-sm">
-      One of three columns
-    </div>
-  </div>
-</div>
-    );
+    
+    )
+  }
+
+}
+
+class Tagbar extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      inputvalue: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event){
+    this.setState({
+      inputvalue: event.target.value,
+    })
+  }
+
+  render(){
+    return(
+      <div className="Tagbar" >
+       <div className="row justify-content-center">
+        <div className ="col-3">
+         <form>
+           <input type="text" class="form-control" placeholder="Search Tags" value={this.state.inputvalue} onChange={this.handleChange}/>
+        </form>
+        </div>
+        <div className="col-1">
+        <button type="button" className="btn btn-primary" onClick={() => this.props.onClick(this.state.inputvalue)}>>Search</button>
+        </div>
+        </div>
+      </div>
+    )
   }
 }
 

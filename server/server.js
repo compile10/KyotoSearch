c0419c4c51a89abe9b2ea9f58ebfd9303b3e48e6
Efrain Gonzalez <efrain800@gmail.com>
2018-07-27 00:11:19 -0700

    Moved fetch to Thumbgrid and fixed data flow

--- server/server.js
+++ server/server.js
@@ -1,10 +1,24 @@
+const fetch = require('node-fetch');
 const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+
+
+app.get('/api/images/:service/', (req, res) => {
+  console.log(`Recieved image GET request for ${req.query.tags}`);
+
+  let url = '';
+  if(req.params.service == "gelbooru"){
+    url = `https://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=100&tags=${req.query.tags}&json=1`
+  }
+
+  fetch(url)
+  .then((Data) => {return Data.json()})
+  .then((Data) => {res.send(Data)})
+
+
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- src/App.js
+++ src/App.js
@@ -1,44 +1,105 @@
 import React, { Component } from 'react';
 import './App.css';
-/*
-function Thumbnail(prop){
-  const imgstyle= { 
-    maxWidth: '150px',
-    maxHeight: '200px',
-    
+
+class Thumbnail extends React.Component {
+  constructor(props) {
+    super(props)
+    this.thumbnailUrl = this.thumbnailUrl.bind(this)
   }
+
+  thumbnailUrl(imageobj){
+    let url = ''
+    if(this.props.service === "gelbooru"){
+       url = `https://simg3.gelbooru.com/thumbnails/${imageobj.directory}/thumbnail_${imageobj.hash}.jpg`
+    }
+    else{
+      return
+    }
+    return url
+  }
+
+  render() {
+   
+    const imgstyle= { 
+      maxWidth: '150px',
+      maxHeight: '200px',
+      
+    }
+
     return(
-      <div>
-      <a href={prop.url}><img style={imgstyle} src={prop.thumb}/></a> 
+      <div class="col">
+        <a href={this.props.imageData.file_url}> <img alt="Thumbnail" style={imgstyle} src={this.thumbnailUrl(this.props.imageData)}/> </a> 
       </div> 
     )
-
+  }
 }
 
 function Row(prop){
-return(
-  <div className="row">
-    <div className="col"><Thumbnail/></div>
-
-  </div>
-)
+  let newRow = [];
+  for( let i = 0; i < 10; i++){
+    if(this.props.imageDataArray.length !== 0){
+     newRow.push( <Thumbnail imageData={this.props.imageArrayData.shift()} key={i}/>)
+    }
+  }
+  return(
+    <div className="row">
+      {newRow} 
+    </div>
+  )
 }
 
-class thumbgrid extends Component{
 
 
+class Thumbgrid extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      loading: true, 
+      imageDataArray: [{}]
+    }
+  }
+
+  componentWillMount(){
+    const Url = `/api/images/${this.props.service}/?tags=${this.props.tag}`
+
+    return fetch(Url)
+      .then((stream) => {return stream.json()})
+      .then((data) => {return data} )
+      .then((data) => { this.setState({
+          imageDataArray: data,
+          loading: false
+      }) })
+
+  }
+
+  
 
   render(){
-    let rows = [];
-    for( let i = 0; i < 10; i++){
-      rows.push( <Row imageData={props.ImageData} key={i}/>)
+      if(this.state.loading === false){
+        let rows = [];
+        let arrayCopy = this.imageDataArray; 
+        for( let i = 0; i < 10; i++){
+          rows.push( <Row imageDataArray={arrayCopy} key={i}/>)
+        }
+        return(
+          <div> {rows} </div>
+        )
+      }
+      else{
+        const loadingtext = {
+          fontStyle: 'italic',
+          textAlign: 'center',
+          paddingTop: '80px',
+        }
+        return (
+          <h1 style={loadingtext}> Loading... </h1>
+        )
+      }
+      
     }
-    return(
-      <div> {rows} </div>
-    )
   }
-}
-*/
+
+
 
 
 class Tagbar extends Component{
@@ -89,9 +150,8 @@ class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      tag: ''
+      tag: '',
     }
-    this.fetchImgs = this.fetchImgs.bind(this);
   }
 
   setTag(tag){
@@ -99,18 +159,7 @@ class App extends Component{
       tag: tag
     })
   }
-  fetchImgs(tag){
-    const Url=`https://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=10&tags=${tag}&json=1`
-    
- 
 
-    fetch(Url)
-    .then((Data) => {return Data.json()})
-    .then((Data) => {console.log(Data[0])})
-
-    
-  
-  }
   render(){
     const hide = {
       display: 'none'
@@ -121,16 +170,14 @@ class App extends Component{
       paddingTop: '80px',
     }
     if(this.state.tag){
-      this.fetchImgs(this.state.tag);
+
     }
     
     return(
-    <div className="App">
+    <div className="container">
        <Tagbar onClick={(tag) => this.setTag(tag)}/> 
-       
        <h1 style={this.state.tag ? hide : show}> Enter a tag to search for images. </h1>
-       
-       
+       { this.state.tag && <Thumbgrid tag={this.state.tag} service="gelbooru"/> }
     </div>
     
     )

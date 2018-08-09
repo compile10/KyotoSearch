06ad2ba4749a020092c52690dbc75f6517955e85
compile10 <efrain800@gmail.com>
2018-08-08 19:00:46 -0700

    Converted gelbooru parser to XML

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const fetch = require('node-fetch');
 const express = require('express');
+const parseString = require('xml2js').parseString;
+
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,10 +11,10 @@ const port = process.env.PORT || 5000;
 
 function parseGelbooru(data){
     let frontData = [];
-    for(let thisImage of data){
+    for(let thisImage of data.posts.post){
       frontData.push({ 
-        thumbURL: `https://simg3.gelbooru.com/thumbnails/${thisImage.directory}/thumbnail_${thisImage.hash}.jpg`,
-        pageURL: `https://gelbooru.com/index.php?page=post&s=view&id=${thisImage.id}`
+        thumbURL: thisImage.$.preview_url,
+        pageURL: `https://gelbooru.com/index.php?page=post&s=view&id=${thisImage.$.id}`
       });
     }
     return JSON.stringify(frontData);
@@ -26,12 +28,18 @@ app.get('/api/images/:service/', (req, res) => {
 
   let url = '';
   if(req.params.service == "gelbooru"){
-    url = `https://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=100&tags=${req.query.tags}&json=1`
+    url = `https://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=100&tags=${req.query.tags}`
   }
 
+  
   fetch(url)
-  .then((Data) => {return Data.json()})
-  .then((Data) => { res.send(parseGelbooru(Data)) })
+  .then( (Data) => {return Data.text()} )
+  .then((Data) => {
+    parseString(Data, (err, result) => {
+      var parsedData = parseGelbooru(result)
+      res.send(parsedData)
+    })
+  })
 
 
 });

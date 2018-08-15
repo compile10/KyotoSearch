389aa77b84fdce39830ad1de0358f8816312f241
Efrain Gonzalez <efrain800@gmail.com>
2018-08-14 17:14:57 -0700

    Moved navigation classes into their own file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Pagination, Tagbar } from './Navigation.js';
 import Eclipse from './Eclipse';
 
-const LEFT_PAGE = -1; 
-const RIGHT_PAGE = -2; 
+
 
 function Thumbnail(props){
     const colStyle = {
@@ -39,153 +39,6 @@ function Thumbgrid(props){
     )
   
   }
-
-
-
-class Pagination extends Component{
-  constructor(props){
-    super(props);
-    this.range = this.range.bind(this);
-    this.pageNumbers = this.pageNumbers.bind(this);
-  }
-  range(bottom, top){
-    let range = []
-    for(let i = bottom; i <= top; i++ ){
-      range.push(i);
-    }
-    return range; 
-  }
-  pageNumbers(currentPage, totalPages){
-    let leftOverflow = true
-    let rightOverflow = true
-
-    if(currentPage <= 3){
-      leftOverflow = false
-    }
-    if(currentPage > totalPages - 3){
-      rightOverflow = false
-    }
-
-    if(totalPages <= 7 ){
-      rightOverflow = false
-      leftOverflow = false
-    }
-
-    let pageNums = []
-
-    switch(true){
-      case (leftOverflow && rightOverflow):
-        pageNums.push(LEFT_PAGE)
-        pageNums = pageNums.concat(this.range(currentPage - 3, currentPage + 3))   
-        pageNums.push(RIGHT_PAGE)
-        break; 
-      case (!leftOverflow && rightOverflow):
-        pageNums = this.range(1,7)
-        pageNums.push(RIGHT_PAGE)
-        break;
-      case (leftOverflow && !rightOverflow):
-        pageNums.push(LEFT_PAGE) 
-        pageNums = pageNums.concat(this.range(totalPages - 7, totalPages)); 
-        break;
-      default:
-        pageNums = this.range(1,totalPages)
-    }
-
-    return pageNums; 
-  }
-
-  render(){
-    let totalPages = Math.floor(this.props.totalImages/100);
-    totalPages = this.props.totalImages % 100 !== 0 ? totalPages + 1 : totalPages;
-    let pageNums = this.pageNumbers(this.props.currentPage, totalPages)
-
-    const paginationArray = pageNums.map( (i) => {
-      switch(i){
-        case this.props.currentPage:
-          return (
-            <li className="page-item active" key={i}>
-              <button type="button"  className="page-link" onClick={() => this.props.onClick(this.props.tags, i) }> {i} </button>
-            </li>
-          )
-        
-        case LEFT_PAGE:
-          return(
-            <li className="page-item" key={LEFT_PAGE}>
-              <button type="button" className="page-link" onClick={() => this.props.onClick(this.props.tags, 1) }>&laquo; </button>
-            </li>
-          )
-        
-        case RIGHT_PAGE:
-          return(
-            <li className="page-item" key={RIGHT_PAGE}>
-              <button type="button" className="page-link" onClick={() => this.props.onClick(this.props.tags, totalPages)}> &raquo; </button>
-            </li>
-          )
-         
-        default: 
-          return(
-            <li className="page-item" key={i}> 
-              <button type="button" className="page-link" onClick={() => this.props.onClick(this.props.tags, i)}>{i}</button> 
-            </li>
-          )
-    }
-    })
-
-    return(
-      <div className="row justify-content-center">
-        <div className="col-lg-2 d-flex col-md-5 col-sm-8">
-          <ul className="pagination mx-auto">
-            {paginationArray}
-          </ul>
-        </div>
-      </div>
-    )
-  }
-
-}
-
-
-
-
-class Tagbar extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      inputvalue: ''
-    }
-    this.handleChange = this.handleChange.bind(this);
-    this.enterKey = this.enterKey.bind(this);
-  }
-  handleChange(event){
-    this.setState({
-      inputvalue: event.target.value,
-    })
-  }
-
-  enterKey(event){
-    if (event.target.type !== 'textarea' && event.which === 13 /* Enter */) {
-      event.preventDefault();
-      this.props.onClick(this.state.inputvalue, 1); 
-}
-  }
-
-  render(){
-    return(
-      <div className="Tagbar" >
-       <div className="row justify-content-center">
-        <div className ="col-lg-4 col-md-5 col-9">
-         <form>
-           <input type="text" className="form-control" placeholder="Search Tags" value={this.state.inputvalue} onChange={this.handleChange } onKeyPress={this.enterKey} />
-        </form>
-        </div>
-        <div className="col-lg-1 col-md-1 col-1">
-        <button type="button" className="btn btn-primary" onClick={() => this.props.onClick(this.state.inputvalue, 1)}>Search</button>
-        </div>
-        </div>
-      </div>
-    )
-  }
-}
 
 
 

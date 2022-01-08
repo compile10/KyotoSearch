54c02d4510fbc931ed73cd7b6429eb03e111b784
compile10 <efrain800@gmail.com>
2022-01-07 16:34:37 -0800

    Updated Gelbooru to new API

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.get('/api/images/:service/', (req, res) => {
 
   switch(req.params.service){
     case 'gelbooru':
-      fetchGelbooru(req.query.tags, req.query.page -1, res, "gelbooru.com", parseGelbooru)
+      fetchNewGelbooru(req.query.tags, req.query.page -1, res, "gelbooru.com", parseGelbooru)
       break;
     case 'danbooru':
       fetchDanbooru(req.query.tags, req.query.page - 1, res, "danbooru.donmai.us", "Danbooru")
@@ -34,7 +34,7 @@ app.get('/api/images/:service/', (req, res) => {
 function parseGelbooru(data, postCount){
   let images = data.map( thisImage => {
     return { 
-      thumbURL: `https://gelbooru.com/thumbnails/${thisImage.directory}/thumbnail_${thisImage.hash}.jpg`,
+      thumbURL: thisImage.preview_url,
       pageURL: `https://gelbooru.com/index.php?page=post&s=view&id=${thisImage.id}`
     };
   })
@@ -151,7 +151,7 @@ function fetchGelbooru(tags, offset, res, domain, parser ){
   Promise
   .all(urls.map(grabContent))
   .then(arrays => arrays.map(array => dataArray.push(...array)) )
-  .catch((error) => {console.log(`Search not found for ${tags}.`)})
+  .catch((error) => {console.log(`Search not found for ${tags} `)})
   .then(() => fetch(`https://${domain}/index.php?page=dapi&s=post&q=index&limit=0&tags=${tags}&json=0&pid=0`) )
   .then((result) => result.text())
   .then((xmlresult) => {
@@ -170,6 +170,42 @@ function fetchGelbooru(tags, offset, res, domain, parser ){
     })
   })
 }
+
+
+function fetchNewGelbooru(tags, offset, res, domain, parser ){
+  let urls = []
+  for(let i = 0; i <= 4; i++){ 
+    urls.push(`https://${domain}/index.php?page=dapi&s=post&q=index&limit=20&tags=${tags}&json=1&pid=${i + (5 * offset)}`)
+  }
+
+  const grabContent = url => fetch(url)
+  .then(res => res.json())
+
+  let dataArray = []
+
+  Promise
+  .all(urls.map(grabContent))
+  .then(arrays => arrays.map(array => dataArray.push(...array.post)) )
+  .catch((error) => {console.log(`Search not found for ${tags} `)})
+  .then(() => fetch(`https://${domain}/index.php?page=dapi&s=post&q=index&limit=0&tags=${tags}&json=0&pid=0`) )
+  .then((result) => result.text())
+  .then((xmlresult) => {
+    parseString(xmlresult, (err, result) => {
+      
+      if(result.posts.$.count === "0"){
+        const noResult = {
+          totalImages: 0
+        }
+        res.send(noResult)
+      }
+      else{
+        var parsedResult = parser(dataArray, result.posts.$.count)
+        res.send(parsedResult)
+      }
+    })
+  })
+}
+
 
 
 
